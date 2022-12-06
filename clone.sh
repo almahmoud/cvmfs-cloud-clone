c6ff7eaebeba29cd4ff5ce5cbee7289c3474c2ab
Enis Afgan <afgane@gmail.com>
2022-12-05 19:11:00 -0500

    Use Docker instead of Helm

---
+++ clone.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -xe
+
+export CVMFS_PATH=/cvmfs/cloud.galaxyproject.org
+export LOCAL_CVMFS_PATH=/full/cvmfs/cloud.galaxyproject.org
+
+printf "Restarting automount"
+service autofs restart
+
+printf "\nTest CVMFS mounted"
+ls -alh $CVMFS_PATH
+
+printf "\nCreate dirs"
+mkdir -p $LOCAL_CVMFS_PATH && \
+mkdir -p /startup/cvmfs/cloud.galaxyproject.org && \
+mkdir -p /partial/cvmfs/cloud.galaxyproject.org
+
+printf "\nSync full tool and config dirs"
+rsync -aP --exclude '*/.hg*' $CVMFS_PATH/tools $LOCAL_CVMFS_PATH/ && \
+rsync -aP $CVMFS_PATH/config $LOCAL_CVMFS_PATH
+
+printf "\nList local CVMFS dir"
+ls -alh $LOCAL_CVMFS_PATH && \
+du -h -d 1 $LOCAL_CVMFS_PATH
+
+printf "\nSync startup dir"
+rsync -amP --exclude='*-data/*' --include='*/' --include='*.xml' --exclude='*' $LOCAL_CVMFS_PATH/tools /startup/cvmfs/cloud.galaxyproject.org/ && \
+rsync -aP $LOCAL_CVMFS_PATH/config /startup/cvmfs/cloud.galaxyproject.org/
+
+printf "\nSync partial dir"
+rsync -amP --exclude='*-data/*' $LOCAL_CVMFS_PATH/tools /partial/cvmfs/cloud.galaxyproject.org/ && \
+rsync -aP $LOCAL_CVMFS_PATH/config /partial/cvmfs/cloud.galaxyproject.org/
+
+printf "\nCreate archives"
+mkdir -p /mnt/archives/ && \
+tar -zcvf /mnt/archives/contents.tar.gz $LOCAL_CVMFS_PATH/ && \
+tar -zcvf /mnt/archives/startup.tar.gz /startup/cvmfs/cloud.galaxyproject.org/ && \
+tar -zcvf /mnt/archives/partial.tar.gz /partial/cvmfs/cloud.galaxyproject.org/
+
+printf "\nCopy files to GCP bucket"
+gsutil -m rsync -r -d /mnt/archives/ gs://cloud-cvmfs/
+
+printf "\nDone."
